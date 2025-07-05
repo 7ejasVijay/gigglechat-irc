44ca42898286e6eb7ad2a7108e5970207b406b46
7ejasVijay <7ejas.vijay@gmail.com>
2025-07-05 17:46:48 +0530

    add: Message structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
-use iroh::{Endpoint, SecretKey, protocol::Router};
+use iroh::{Endpoint, SecretKey, protocol::Router, NodeId};
 use iroh_gossip::{net::Gossip, proto::TopicId};
+use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -19,11 +20,45 @@ async fn main() -> Result<()> {
     // Get the GossipSender and the GossipReceiver
     let (mut sender, _receiver) = topic.split();
 
+    let message = Message::new(MessageBody::AboutMe {
+        from: endpoint.node_id(),
+        name: String::from("alice")
+    });
+
     // Broadcast a message to the topic
-    sender.broadcast("sup".into()).await?;
+    sender.broadcast(message.to_vec().into()).await?;
 
     // Cleanly shutdown the router
     router.shutdown().await?;
 
     Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    body: MessageBody,
+    nonce: [u8; 16],
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum MessageBody {
+    AboutMe { from: NodeId, name: String },
+    Message { from: NodeId, text: String },
+}
+
+impl Message {
+    pub fn new(body: MessageBody) -> Self {
+        Self {
+            body,
+            nonce: rand::random(),
+        }
+    }
+
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        serde_json::from_slice(bytes).map_err(Into::into)
+    }
+
+    pub fn to_vec(&self) -> Vec<u8> {
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
 }
