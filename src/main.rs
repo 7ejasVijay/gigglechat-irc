433e58a915d5d6a9be988e73c7e74305f385a26d
7ejasVijay <7ejas.vijay@gmail.com>
2025-07-05 17:27:45 +0530

    add: creating and broadcasting to a topic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use iroh::{Endpoint, SecretKey, protocol::Router};
-use iroh_gossip::net::Gossip;
+use iroh_gossip::{net::Gossip, proto::TopicId};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -8,6 +8,20 @@ async fn main() -> Result<()> {
     let endpoint = Endpoint::builder().secret_key(secret_key).discovery_n0().bind().await?;
     let gossip = Gossip::builder().spawn(endpoint.clone());
     let router = Router::builder(endpoint.clone()).accept(iroh_gossip::ALPN, gossip.clone()).spawn();
+
+    // Create a new topic
+    let id = TopicId::from_bytes(rand::random());
+    let node_ids = vec![];
+
+    // Subscribe to the topic
+    let topic = gossip.subscribe(id, node_ids).await?;
+    
+    // Get the GossipSender and the GossipReceiver
+    let (mut sender, _receiver) = topic.split();
+
+    // Broadcast a message to the topic
+    sender.broadcast("sup".into()).await?;
+
     // Cleanly shutdown the router
     router.shutdown().await?;
 
